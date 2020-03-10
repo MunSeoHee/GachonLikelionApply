85eaffda74081fb7bffee06b3baab27782f5f8c0
ToA <nansh9815@naver.com>
2020-03-10 22:59:53 +0900

    user id check

--- member_insert.php
+++ member_insert.php
@@ -7,15 +7,21 @@
     $result = mysqli_query($con, $sql);
     $res = mysqli_num_rows($result);
 
+    //아이디가 DB에 있을 경우
     if($res){
         echo "<script> alert('이미 존재하는 아이디입니다.');</script>";
+        //이전 페이지로 돌아가기
         echo "<script> window.history.back();</script>";
         exit();
     }
+    //아이디가 DB에 없을 경우
     else{
+        //DB에 user 정보 삽입
         $sql = "insert into user (id, pw) values ('$id', '$pw')";
         $result = mysqli_query($con, $sql);
+        //결과가 성공적일 경우
         if($result){
+            //가입 완료 안내 후 main 페이지로 이동
             echo "<script> alert('가입 되었습니다.');</script>";
             echo " 
             <script>
@@ -23,7 +29,9 @@
             </script>
         ";
         }
+        //실패했을 경우
         else{
+            //실패 안내 후 이전 페이지로 
             echo "<script> alert('가입에 실패하였습니다.');</script>";
             echo "<script> window.history.back();</script>";
             exit();
--- signin_page.php
+++ signin_page.php
@@ -13,23 +13,14 @@
         }
         document.member.submit();
     }
-    function check_id(){
-        window.open("user_id_check.php?id="+document.member.id.value, "IDcheck", "left=700, top=300, width=350, height=200, scrollbars=no, resizable=yes");
-    }
-    function chkForm(){
-        var checkid=document.all.checkid.value;
-        if(checkid==0){
-            alert("ID 중복체크를 하세요!");
-            return false;
-        }
-        return true;
-    }
 </script>
 
+<!--회원가입 정보 입력 란-->
 <form name="member" method="post" action="member_insert.php">
+    <!--id 입력 칸-->
     <input type="text" name="id">
-    <div onclick="check_id()">중복체크</div>
-    <input type="hidden" name="checkid" value=0>
+    <!--비밀번호 입력 칸-->
     <input type="password" name="pw">
+    <!--제출 버튼-->
     <div style="cursor:pointer" onclick="check_input()">제출</div>
 </form>
