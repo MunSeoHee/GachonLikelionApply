c1439efc14632f4e4966b54bdd4af2c19d589f13
ToA <nansh9815@naver.com>
2020-03-10 16:51:51 +0900

    id check

--- signin_page.php
+++ signin_page.php
@@ -13,10 +13,14 @@
         }
         document.member.submit();
     }
+    function check_id(){
+        window.open("user_id_check.php?id="+document.member.id.value, "IDcheck", "left=700, top=300, width=350, height=200, scrollbars=no, resizable=yes");
+    }
 </script>
 
 <form name="member" method="post" action="member_insert.php">
     <input type="text" name="id">
+    <div onclick="check_id()">중복체크</div>
     <input type="password" name="pw">
     <div style="cursor:pointer" onclick="check_input()">제출</div>
 </form>
---
+++ user_id_check.php
@@ -0,0 +1,24 @@
+<h1>아이디 중복 체크<h1>
+<p>
+    <?php
+        include_once 'settings.php';
+        $id = $_GET["id"];
+        if(!$id){
+            echo("아이디를 입력해주세요");
+        }
+        else{
+            $sql = "select * from user where id='$id'";
+            $resutl = mysqli_query($con, $sql);
+            $res = mysqli_num_row($result);
+
+            if($res){
+                echo "아이디가 중복됩니다<br>다른 아이디를 사용해주세요";
+            }
+            else{
+                echo "아이디 사용이 가능합니다";
+            }
+        }
+    ?>
+<div>
+    <div onclick="javascript:self.close()">;
+</div>
