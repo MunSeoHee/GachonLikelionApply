797a47f7e6800cc4ccbae2c639e55e78fe1d1719
ToA <nansh9815@naver.com>
2020-03-10 15:51:31 +0900

    user insert

--- member_insert.php
+++ member_insert.php
@@ -6,6 +6,13 @@
     $sql = "insert into user (id, pw) values ($id, $pw)";
     $result = mysqli_query($con, $sql);
     
+    if($con){
+        echo "connect : 성공<br>";
+    }
+    else{
+        echo "disconnect : 실패<br>";
+    }
+    
     if($result){
     echo "쿼리성공";
     }else{
