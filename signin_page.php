4b39c7c29b74e71475e09edf6b32207dec25038d
ToA <nansh9815@naver.com>
2020-03-10 16:15:24 +0900

    id pw check

--- signin_page.php
+++ signin_page.php
@@ -5,11 +5,18 @@
             document.member.id.focus();
             return;
         }
+
+        if(!document.member.pw.value){
+            alert("비밀번호를 입력해주세요");
+            document.member.pw.focus();
+            return;
+        }
+        document.member.submit();
     }
 </script>
 
 <form name="member" method="post" action="member_insert.php">
     <input type="text" name="id">
     <input type="password" name="pw">
-    <button type="submit">제출</button>
+    <button type="submit" onclick="check_input()">제출</button>
 </form>
