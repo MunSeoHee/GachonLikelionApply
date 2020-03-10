fb2b873fe5585911b7995967235067c88453ceaa
ToA <nansh9815@naver.com>
2020-03-10 15:36:52 +0900

    user insert

---
+++ member_insert.php
@@ -0,0 +1,11 @@
+<?php
+    include_once 'settings.php';
+    $id = $_POST["id"];
+    $pw = $_POST["pw"];
+    
+    $sql = "insert into user (id, pw) values ($id, $pw)";
+    mysqli_query($con, $sql);
+    mysqli_close();
+
+
+?>
