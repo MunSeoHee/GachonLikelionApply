99ea767380ebe6432481255111f12404cc260a21
ToA <nansh9815@naver.com>
2020-03-12 15:24:51 +0900

    login

---
+++ login_page.php
@@ -0,0 +1,21 @@
+<script>
+    function check_input(){
+        if(document.login.id.value){
+            alert("아이디를 입력하세요");
+            document.login.id.focus();
+            return;
+        }
+        if(document.login.pw.value){
+            alert("비밀번호를 입력하세요");
+            document.login.pw.focus();
+            return;
+        }
+        documeny.login.submit();
+    }
+</script>
+
+<form name="login" method="post" action="login.php">
+    <input type="text" name="id" placeholder="아이디">
+    <input type="password" name="pw" placeholder="비밀번호">
+    <div onclick="check_input()">로그인</div>
+<form>
