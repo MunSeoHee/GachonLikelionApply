6f07fdef0f635824423e8155373754e007cc35a5
SeongyonhShin <ssystarsky@naver.com>
2020-03-12 19:04:09 +0900

    no message

--- servertimetest.php
+++ servertimetest.php
@@ -8,8 +8,8 @@ echo "현재 날짜 : ". date("Y-m-d")."<br/>";
 echo "현재 시간 : ". date("H:i:s")."<br/>";
 echo "현재 일시 : ". date("Y-m-d H:i:s")."<br/>";
 if(date("h" == "7")){
-alert("hi");
+    echo "<script> alert('hi');</script>";
 }else{
-    alert("bye");
+    echo "<script> alert('bye');</script>";
 }
 ?>
