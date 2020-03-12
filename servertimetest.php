ac3223192c166a2150e6f3de0aaac6f6bfcf296e
SeongyonhShin <ssystarsky@naver.com>
2020-03-12 19:07:31 +0900

    no message

--- servertimetest.php
+++ servertimetest.php
@@ -3,7 +3,7 @@
     ?>
     <h1>현재 서버시간 : </h1>
     <?php
-echo "asd";
+    date_default_timezone_set('Asia/Seoul');
 echo "현재 날짜 : ". date("Y-m-d")."<br/>";
 echo "현재 시간 : ". date("H:i:s")."<br/>";
 echo "현재 일시 : ". date("Y-m-d H:i:s")."<br/>";
