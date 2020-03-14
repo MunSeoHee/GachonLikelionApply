d7adebd28e3715fde0cb61230feb17e3148cb236
SeongyonhShin <ssystarsky@naver.com>
2020-03-14 17:36:13 +0900

    no message

---
+++ srtt.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ko">
+
+<head>
+    <meta charset="utf-8">
+
+</head>
+<title>PHP</title>
+
+<body>
+
+<form action = "stt.php" method="post">
+    <label>first testpage</label>
+    <input type="submit"value="RUN" />
+
+
+</form>
+</body>
+</html> 
--- stt.php
+++ stt.php
@@ -2,6 +2,11 @@
 <head>
 </head>
 <body>
+
+    <?php
+        include "servertimecheck.php"
+    ?>
+
     <input type="button" id = "test" value="click" onclick="button1_click();" method = "post">
 </body>
 </html>
