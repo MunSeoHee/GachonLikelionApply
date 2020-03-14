8d6afdd90b05cc1c4d22463e99cfb1ffdcbcaaeb
jasonoh22 <jasonoh22@naver.com>
2020-03-14 16:08:27 +0900

    1

--- nav.php
+++ nav.php
@@ -3,6 +3,29 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<style>
+    .bgimg {
+    border: 0;
+    padding: 0; 
+    background-image: url(\lion.jpg);
+    min-height: 100%;
+    background-position: center;
+    background-size: cover;
+    background-repeat : no-repeat;
+		}
+     @font-face { font-family: 'GmarketSansBold'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff%27') format('woff'); font-weight: normal; font-style: normal; }
+        @font-face { font-family: 'yg-jalnan'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_four@1.2/JalnanOTF00.woff%27') format('woff'); font-weight: normal; font-style: normal; }
+        h2,a{
+            color:rgba(243, 212, 73, 0.904);
+            font-family:'GmarketSansBold '; 
+            text-decoration: none;
+        }
+        p{
+            font-size: 120%;
+            color:rgba(243, 212, 73, 0.904);
+            font-family: 'yg-jalnan'; 
+        }
+      </style> 
 </head>
 <body>
 <?php
