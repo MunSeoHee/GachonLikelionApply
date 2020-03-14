23cc0570be5a5a6da47486058c4f5d789449ca60
SeongyonhShin <ssystarsky@naver.com>
2020-03-14 17:30:35 +0900

    no message

--- servertimecheck.php
+++ servertimecheck.php
@@ -1,7 +1,7 @@
 <!--<h1>현재 서버시간 : </h1>-->
 <?php
 // $check = $_POST["check"];
-// echo "테스트 제대로 되나? : $check <br>";
+echo "테스트 제대로 되나? : $check <br>";
 $prevPage = $_SERVER['HTTP_REFERER'];
 date_default_timezone_set('Asia/Seoul');
 //echo "<script> alert('1 받음.');</script>";
