efee13492a868a98d1da596b7a36dd201d39dcc1
SeongyonhShin <ssystarsky@naver.com>
2020-03-14 17:11:15 +0900

    no message

--- servertimeresulttest.php
+++ servertimeresulttest.php
@@ -18,13 +18,23 @@
 <body>
     <h1>현재 서버시간 : </h1>
     
-<form action="servertimetest.php" method="post">
+<form method="post">
     <label>first testpage</label>
     <?php
     //  $check = "1";
     //  $ck["check"] = $check;
      ?>
-    <input type="button" value="클릭ㅋ" onclick="button1_click()">
+    <input type="submit" name="test" id="test" value="RUN" />
+
+
 </form>
 </body>
 </html> 
+<?php 
+function testfun() {
+     echo "Your test function on button click is working"; 
+        } 
+            if(array_key_exists('test',$_POST)){ testfun(); 
+        }
+      ?>
+
