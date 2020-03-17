749905267cbc65dd44b0a7a2401809b4632b0c5e
ToA <nansh9815@naver.com>
2020-03-17 16:49:07 +0900

    nav

--- nav.php
+++ nav.php
@@ -55,14 +55,14 @@
                     if(!$email){
                 ?>
                         <div class="col d-flex justify-content-end"><a href="login_page.php">로그인</a></div>
-                        <div class="col d-flex justify-content-start"><a href="signin_page.php">회원가입</a></div>
+                        <div class="col-lg-3 d-flex justify-content-start"><a href="signin_page.php">회원가입</a></div>
                         
                         
                 <?php
                     }
                     else{
                 ?>      <div class="col d-flex justify-content-end"><?=$email?>님</div>
-                        <div class="col d-flex justify-content-start"><a href="logout.php">로그아웃</a></div>
+                        <div class="col-lg-3 d-flex justify-content-start"><a href="logout.php">로그아웃</a></div>
                 <?php
                     }
                 ?>
