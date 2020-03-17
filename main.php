2124ace5ab3d087f7e1ae82acec70e202fbd5970
SeongyonhShin <ssystarsky@naver.com>
2020-03-17 14:33:52 +0900

    메인 서버시간 실시간

--- main.php
+++ main.php
@@ -160,10 +160,11 @@ body {
         <li class="ghost">처</li>
         <li class="ghost">럼</li>
         <br><br><br><br>
+        <div id="server_time" >현재 서버시간 : <?php echo date("Y-m-d H:i:s", time()); ?></div>
         <li class="ghost pl-5 ml-5"> <button type="button"  class="btn btn-dark ghost ml-5"onclick="apply()">APPLY</button></li>
         
     <br> <br> <br><br><br>
-    <div id="server_time" ><?php echo date("Y-m-d H:i:s", time()); ?></div>
+    
     <p  font-size: 5em; >
       모집 : 2020.00.00~2020.00.00 00:00 <br>
       서류 및 면접 : 2020.00.00~2020.00.00 <br>
