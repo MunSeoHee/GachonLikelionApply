85d6184bd2301052c045c3cf2e4e60307d0c2957
ToA <nansh9815@naver.com>
2020-03-17 15:06:39 +0900

    index

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
     <meta charset="utf-8">
+    <!-- 모바일 뷰포트 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, 
+		maximum-scale=1.0, minimum-scale=1.0">
   <style>
       /* FAQ */
     .faq{border-bottom:0px solid #ddd;margin:1em 0;}
