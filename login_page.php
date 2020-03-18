076ebf18e1feeb9c17b0f6ed1c1870effb5fb991
ToA <nansh9815@naver.com>
2020-03-18 23:52:23 +0900

    login page

--- login_page.php
+++ login_page.php
@@ -33,8 +33,8 @@
     @font-face { font-family: 'MapoHongdaeFreedom'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/MapoHongdaeFreedomA.woff') format('woff'); font-weight: normal; font-style: normal; }
      	@font-face { font-family: 'GmarketSansBold'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff') format('woff'); font-weight: normal; font-style: normal; }
     	@font-face { font-family: 'yg-jalnan'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_four@1.2/JalnanOTF00.woff') format('woff'); font-weight: normal; font-style: normal; }
-	h2,label,h1{
-            color:rgba(243, 212, 73, 0.904);
+	h2,label,h1, a{
+            /* color:rgba(243, 212, 73, 0.904); */
             font-family:'GmarketSansBold'; 
         }
         p{
