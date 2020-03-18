0fee77b02e9c14e4300ce3ca04a1caa7c5f88850
ToA <nansh9815@naver.com>
2020-03-19 02:03:29 +0900

    footer

--- index.php
+++ index.php
@@ -228,10 +228,8 @@
         
   </div>
   <footer>
-    <?php 
-      include_once "footer.html"; 
-    ?>
-    <div class="row d-flex justify-content-end p-0 container-fluid" style="background-color: black; font-family: none;">
+  <div class="row d-flex justify-content-end p-3 container-fluid" style="background-color: black; font-family: none;">
+    <div class="text-white">Copyright 2020. GachonLikelion, All Rights Reserved.</div>
     <div class="text-white">video by 멋쟁이사자처럼 한국항공대학교 </div>
     </div>
 </footer>
