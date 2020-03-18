bdedf78a7a34ddfdf5af591f3b64e68785eb72a5
ToA <nansh9815@naver.com>
2020-03-18 23:41:38 +0900

    nav

--- nav.php
+++ nav.php
@@ -1,26 +1,8 @@
-<head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <style>
     a:link { color: white; text-decoration: none;}
     a:visited { color: white; text-decoration: none;}
     a:hover { color: white!important; text-decoration: none!important;}
-     @font-face { font-family: 'GmarketSansBold'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff%27') format('woff'); font-weight: normal; font-style: normal; }
-        @font-face { font-family: 'yg-jalnan'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_four@1.2/JalnanOTF00.woff%27') format('woff'); font-weight: normal; font-style: normal; }
-        h2,a{
-            color:rgba(243, 212, 73, 0.904);
-            font-family:'GmarketSansBold '; 
-            text-decoration: none;
-        }
-        p{
-            font-size: 120%;
-            color:rgba(243, 212, 73, 0.904);
-            font-family: 'yg-jalnan'; 
-        }
       </style> 
-</head>
 <body>
 <?php
     session_start();
@@ -73,4 +55,3 @@
     </div>
    
 </nav>
-</body>
