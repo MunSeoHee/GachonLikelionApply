2de16c90e31d43518ecd4acd58318b4be381ea0c
ToA <nansh9815@naver.com>
2020-03-20 15:47:14 +0900

    지원서 수정 기능

--- form_insert.php
+++ form_insert.php
@@ -19,27 +19,57 @@
     // echo $question3;
     $date = date("Y-m-d H:i:s");
     include_once "settings.php";
-
-    $sql="insert into written (question1, question2, question3, question4, question5, question6, question7, question8, date, email)
-            values ('$question1', '$question2', '$question3', '$question4', '$question5', '$question6', '$question7', '$question8', '$date', '$email')";
-
+    $sql = "select * from written where email='$email'";
     $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result);
+    if(!$row){
+        $sql="insert into written (question1, question2, question3, question4, question5, question6, question7, question8, date, email)
+        values ('$question1', '$question2', '$question3', '$question4', '$question5', '$question6', '$question7', '$question8', '$date', '$email')";
 
-    if($result){
-        echo("
-            <script>
-                location.href = 'result.php';
-            </script>
-        ");
+        $result = mysqli_query($con, $sql);
+
+        if($result){
+            echo("
+                <script>
+                    location.href = 'result.php';
+                </script>
+            ");
+        }
+        else{
+            echo("
+                <script>
+                    window.alert('지원서 작성에 실패했습니다.')
+                    history.go(-1)
+                </script>
+            ");
+        
+        }
+    }else{
+        $sql="update written set 
+        question1='$question1', question2='$question2', question3='$question3', question4='$question4', 
+        question5='$question5', question6='$question6', question7='$question7', question8='$question8', date='$date'
+        where email=='$email';"
+
+        $result = mysqli_query($con, $sql);
+
+        if($result){
+            echo("
+                <script>
+                    location.href = 'result.php';
+                </script>
+            ");
+        }
+        else{
+            echo("
+                <script>
+                    window.alert('지원서 작성에 실패했습니다.')
+                    history.go(-1)
+                </script>
+            ");
+        
+        }
     }
-    else{
-        echo("
-            <script>
-                window.alert('지원서 작성에 실패했습니다.')
-                history.go(-1)
-            </script>
-        ");
-       
-    }
+
+   
 
 ?>
