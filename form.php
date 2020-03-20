ff11ffe47a17d8277be87eb81cf5e6653e8ef5ef
ToA <nansh9815@naver.com>
2020-03-20 15:27:39 +0900

    지원서 수정 기능

--- form.php
+++ form.php
@@ -342,6 +342,9 @@ $('.bytes8').text(getTextLength8(text));
       $res = mysqli_num_rows($result);
       $row = mysqli_fetch_array($result);
     ?>
+    <script>
+      $("exampleFormControlSelect1")val("<?=$row['question7']?>").attr("selected", "selected");
+    </script>
    
 			<div class="row d-flex justify-content-center">
 				
@@ -408,27 +411,9 @@ $('.bytes8').text(getTextLength8(text));
   <div class="form-group mt-5">
     <label for="exampleFormControlSelect1"class="text-warning"><p>면접희망 날짜</p></label>
     <select class="form-control" id="exampleFormControlSelect1" name="question7">
-      <?php
-        if($row['question7']=='4/1'){
-      ?>
-      <option selected value="4/1">4월1일</option>
-      <option value="4/2">4월2일</option>
-      <option value="4/3">4월3일</option>
-      <?php}?>
-      <?php
-        else if($row['question7']=='4/2'){
-      ?>
-      <option value="4/1">4월1일</option>
-      <option selected value="4/2">4월2일</option>
-      <option value="4/3">4월3일</option>
-      <?php}?>
-      <?php
-        else if($row['question7']=='4/3'){
-      ?>
       <option value="4/1">4월1일</option>
       <option value="4/2">4월2일</option>
-      <option selected value="4/3">4월3일</option>
-      <?php}?>
+      <option value="4/3">4월3일</option>
     </select>
     </div>
  <!--8번질문-->
