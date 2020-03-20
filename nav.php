e4e4d30b37c2c834d54e4235d68b08a4fa1b5068
ToA <nansh9815@naver.com>
2020-03-20 18:19:44 +0900

    nav

--- nav.php
+++ nav.php
@@ -42,15 +42,15 @@
                 <?php
                     if(!$name){
                 ?>
-                        <a class="mr-3" href="login_page.php">로그인</a>
-                        <a class="mr-3" href="signin_page.php">회원가입</a>
+                        <a class="mr-3" href="login_page">로그인</a>
+                        <a class="mr-3" href="signin_page">회원가입</a>
                         
                         
                 <?php
                     }
                     else{
-                ?>      <a class="mr-3" href="#"><?=$name?>님</a>
-                        <a class="mr-3" href="logout.php">로그아웃</a>
+                ?>      <a class="mr-3" href="form"><?=$name?>님</a>
+                        <a class="mr-3" href="logout">로그아웃</a>
                 <?php
                     }
                 ?>
