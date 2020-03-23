cabaca6713a6a006adccaa2347d4a78c8a741704
ToA <nansh9815@naver.com>
2020-03-23 14:52:33 +0900

    에널리틱스

--- nav.php
+++ nav.php
@@ -65,6 +65,6 @@
 <script type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
 <script type="text/javascript">
 if(!wcs_add) var wcs_add = {};
-wcs_add["wa"] = "5011b199caf948";
+wcs_add["wa"] = "50ac3c9fc75bb8";
 wcs_do();
 </script>
