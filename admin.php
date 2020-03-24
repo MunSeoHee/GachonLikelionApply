5e8b66e61cf6f5d05b9e4a07e3e5e3d76cff949a
ToA <nansh9815@naver.com>
2020-03-24 15:09:50 +0900

    지원서 목록 페이지

--- admin.php
+++ admin.php
@@ -7,9 +7,9 @@
 </head>
 <?php
     include_once "settings.php";
-    $sql = "select email from written";
+    $sql = "select email from written;";
     $result = mysqli_query($con, $sql);
-    $row = mysqli_fetch_array($result);
+    $row = mysqli_num_rows($result);
     foreach($row as $email){
         echo $email;
     }
