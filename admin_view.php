d188fe347ee22227928b51b0ea99344a47d56f32
ToA <nansh9815@naver.com>
2020-03-24 15:39:42 +0900

    지원서 보는 페이지

--- admin_view.php
+++ admin_view.php
@@ -19,27 +19,29 @@
     <body>
         <div class="container">
             <div class="row">
-            <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                    개인정보
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        개인정보
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">이름 : <?=$user['name']?></li>
+                        <li class="list-group-item">학과 : <?=$user['major']?></li>
+                        <?php
+                                            if($user["gender"] == 1){
+                                                echo "<li class='list-group-item'>성별 : 남자</li>";
+                                            }else{
+                                                echo "<li class='list-group-item'>성별 : 여자</li>";
+                                            }
+                                        ?>
+                        <li class="list-group-item">복수전공 : <?=$user['doublemajor']?></li>
+                        <li class="list-group-item">입학년도 : <?=$user['year']?></li>
+                        <li class="list-group-item">학년 : <?=$user['degree']?></li>
+                        <li class="list-group-item">연락처 : <?=$user['phone']?></li>
+                        <li class="list-group-item">이메일 : <?=$user['email']?></li>
+                    </ul>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">이름 : <?=$user['name']?></li>
-                    <li class="list-group-item">학과 : <?=$user['major']?></li>
-                    <?php
-                                        if($user["gender"] == 1){
-                                            echo "<li class='list-group-item'>성별 : 남자</li>";
-                                        }else{
-                                            echo "<li class='list-group-item'>성별 : 여자</li>";
-                                        }
-                                    ?>
-                    <li class="list-group-item">복수전공 : <?=$user['doublemajor']?></li>
-                    <li class="list-group-item">입학년도 : <?=$user['year']?></li>
-                    <li class="list-group-item">학년 : <?=$user['degree']?></li>
-                    <li class="list-group-item">연락처 : <?=$user['phone']?></li>
-                    <li class="list-group-item">이메일 : <?=$user['email']?></li>
-                </ul>
             </div>
+            <div class="row">
                 <div class="card">
                     <div class="card-header">
                     지원동기 (700자 이내)
@@ -49,7 +51,9 @@
                         <p><?=$row['question1']?></p>
                         </blockquote>
                     </div>
-                </div>
+                </div>                   
+            </div>
+            <div class="row">
                 <div class="card">
                     <div class="card-header">
                     만들고 싶은 서비스를 적어주세요 (1000자 이내)
@@ -60,6 +64,8 @@
                         </blockquote>
                     </div>
                 </div>
+            </div>
+            <div class="row">
                 <div class="card">
                     <div class="card-header">
                     다뤄본 컴퓨터 언어를 실력 상, 중, 하를 포함하여 모두 적어주세요.<br>또한 디자인에 대한 경험이 있으실 경우, 그에 대한 내용과 다뤄본 디자인 툴을 실력 상, 중, 하를 포함하여 모두 적어주세요
@@ -70,6 +76,8 @@
                         </blockquote>
                     </div>
                 </div>
+            </div>
+            <div class="row">
                 <div class="card">
                     <div class="card-header">
                     멋사에서 활동하게 된다면 멋사를 통해 얻어가고 싶은 것과 팀을 위해 어떤 노력 및 기여를 할 수 있는지 적어주세요 (1000자 이내)
@@ -80,6 +88,8 @@
                         </blockquote>
                     </div>
                 </div>
+            </div>
+            <div class="row">
                 <div class="card">
                     <div class="card-header">
                     2020년 한 해 동안 참여하거나 참여할 예정인 동아리, 아르바이트, 대외활동 및 프로젝트, 인턴쉽 프로그램, 공모전 등을 적어주세요
@@ -90,6 +100,8 @@
                         </blockquote>
                     </div>
                 </div>
+            </div>
+            <div class="row">
                 <div class="card">
                     <div class="card-header">
                     가천대학교 멋쟁이 사자처럼 8기는 매주 2회, 목요일 6시 및 토요일 3시 이후에 정규 스터디 및 팀 프로젝트를 진행할 예정입니다. 목요일, 토요일 정기 세션에 참여가 가능하신가요? 해당 요일에 일정이 있거나 일정이 생길 수도 있다면 이에 대한 내용을 적어주세요
@@ -100,6 +112,8 @@
                         </blockquote>
                     </div>
                 </div>
+            </div>
+            <div class="row">
                 <div class="card">
                     <div class="card-header">
                     면접희망 날짜
@@ -110,7 +124,9 @@
                         </blockquote>
                     </div>
                 </div>
-                <div class="card">
+            </div>
+            <div class="row">
+            <div class="card">
                     <div class="card-header">
                     깃허브 주소
                     </div>
@@ -120,7 +136,8 @@
                         </blockquote>
                     </div>
                 </div>
-            </div>  
+            </div>
+ 
         </div>
     </body>
 </html>
