c929cc763c6cb47495dac09c0e5b134c62fff46a
ToA <nansh9815@naver.com>
2020-03-29 18:06:43 +0900

    user

---
+++ user.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<head>
+</head>
+<?php
+    include_once "settings.php";
+    $sql = "select * from user";
+    $result = mysqli_query($con, $sql);
+?>
+
+<body>
+    <div class="container">
+        <div class="row mt-3">
+            <?php
+                while ( $row = mysqli_fetch_array($result) ) {
+                    echo $row['name']." ".$row['gender']." ".$row['year']." ".$row['phone']." ".$row['degree'].$row['doublemajor']."<br>";
+           
+            }?>
+        </div>
+
+    </div>
+</body>
+</html>
